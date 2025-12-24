df8ff435f83dd24bfc3b27c10a9998530737f4f4
Ahmad Raka Putra Pratamaa <2400016089@webmail.uad.ac.id>
2025-12-24 14:09:01 +0700

    Fix deployment path to Praktikum-9

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // 1. Mengaktifkan fitur compiler React (bawaan dari config-mu sebelumnya)
-  reactCompiler: true,
-
-  // 2. Memberitahu Next.js bahwa kita ingin hasil akhirnya berupa file statis (HTML/CSS/JS)
-  output: 'export',
-
-  // 3. Menangani sub-path di GitHub Pages
-  // Karena repo kamu bernama 'my-blog-app', URL-nya nanti akan menjadi:
-  // https://2400016067-max.github.io/my-blog-app/
-  basePath: '/praktikum-9-my-blog-app',
-
-  // 4. Mematikan optimasi gambar bawaan Next.js
-  // GitHub Pages tidak punya server untuk memproses gambar secara dinamis
+  output: "export", // Menghasilkan folder 'out'
+  basePath: "/Praktikum-9", // HARUS sama dengan nama repository
+  assetPrefix: "/Praktikum-9", // HARUS sama dengan nama repository
   images: {
     unoptimized: true,
   },
